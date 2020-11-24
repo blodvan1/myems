d3ccf80b43633ecf3837d7b9a740483f35162e0d
lh <fatgrandmother@hotmail.com>
2020-11-24 14:23:00 +0800

    unexpected indent urls fixed

--- myems/urls.py
+++ myems/urls.py
@@ -44,9 +44,9 @@ urlpatterns = [
 ]
 
 
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
 
 if settings.DEBUG:
 
