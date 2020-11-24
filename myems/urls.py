24694c64d6797836d30a1aae86629b8838dd914d
lh <fatgrandmother@hotmail.com>
2020-11-24 14:21:09 +0800

    dg search now displays images

--- myems/urls.py
+++ myems/urls.py
@@ -21,7 +21,7 @@ from . import views
 
 from django.conf.urls.static import static
 from django.conf import settings
-#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 admin.site.site_header = "Schneider Administration"
@@ -44,8 +44,12 @@ urlpatterns = [
 ]
 
 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
+
 if settings.DEBUG:
 
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    #urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += staticfiles_urlpatterns()
